a08d2371614232b7cbeefdbb5d73bd8a7306fbc5
Fernando Almeida <feralana@hotmail.com>
2024-08-29 12:28:10 -0300

    Condicional if
    Adicionado ao repositório a condicional if

--- Estruturas_repeticao.py
+++ Estruturas_repeticao.py
@@ -41,3 +41,47 @@ convidados = ["Maria", "Gabriel", "Fernando", "Fernanda", "Vinicius", "Ana",  "L
 
 for convidado in convidados:
     print(f"Seja muito bem vindo(a) {convidado}")
+
+print("*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*")
+
+# Estrutura condicional - Comando if
+
+"""
+Operadores Aritiméticos 
+- soma: +
+- subtração: -
+- multiplicação: *
+- divisão: /
+- potenciação: **
+
+Operadores Relacionais
+- igual: ==
+- maior: >
+- maior igual: >=
+- menor: <
+- menor igual: <=
+
+"""
+
+idade = 18
+
+if idade >= 18:
+    print("Pode dirigir!")
+
+idade = 10
+
+if idade >= 18:
+    print("Pode dirigir!")
+else:
+    print("Não pode dirigir!")
+
+print("*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*")
+
+idades = [12, 45, 23, 67, 5, 6, 90, 2, 3, 34, 25, 17, 19]     # idade compatível para consumo de bebida alcoólica
+
+for idade in idades:
+    print(f"Idade: {idade} anos: ")
+    if idade >= 21:
+        print("Pela sua idade, você pode fazer consumo de bebida alcoólica. Aprecie com moderação!")
+    else:
+        print("Sua idade não permite que você faça uso de bebida alcoólica.Respeite as leis de consumo!")
